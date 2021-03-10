84126583f4f1a8ced6621a7ff33bf95b1e3117fe
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-03-10 09:32:10 -0600

    Models for orden

---
+++ src/models/orden.py
@@ -0,0 +1,31 @@
+class Orden:
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def set_nit(self, nit):
+        self.__nit = nit
+
+    def get_nit(self):
+        return self.__nit
+
+    def set_address(self, address):
+        self.__address = address
+
+    def get_address(self):
+        return self.__address
+
+    def set_tip(self, tip):
+        self.__tip = tip
+
+    def get_tip(self):
+        return self.__tip
+
+    def set_item(self, item):
+        self.__items.append(item)
+
+    def get_items(self):
+        return self.__items
---
+++ src/models/orden_item.py
@@ -0,0 +1,13 @@
+class OrdenItem:
+
+    def set_quantity(self, quantity):
+        self.__quantity = quantity
+    
+    def get_quantity(self):
+        return self.__quantity
+
+    def set_item_id(self, item_id):
+        self.__item_id = item_id
+
+    def get_item_id(self):
+        return self.__item_id
