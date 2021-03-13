a85fb5399e67d2b729cf5b3d3a18ada1c19c9dd4
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-03-12 20:21:20 -0600

    Automata working - still missing error states

--- src/automatas/automata.py
+++ src/automatas/automata.py
@@ -4,9 +4,7 @@ class Automata:
 
         charcodes = [39, 91, 93, 58, 59, 61, 44, 37, 46, 95]
         charcodes_main = [91, 93, 58, 59, 61, 44, 37]
-        charcodes_after_string = [93, 58, 59, 44]
-        charcodes_after_id = [61, 59, 58, 44]
-        charcodes_after_digit = [59, 58, 44, 37]
+        charcodes_continue = [39, 91, 93, 58, 59, 61, 44, 37, 10, 32]
 
         with open(file_to_read, 'r') as file:
             data = file.read()
@@ -16,247 +14,193 @@ class Automata:
             column = 0
             column_tk = 0
             temp = ""
-            for char in data:
+            i = 0
+
+            while i < len(data):
 
                 if state == 0:
                     ## Caracteres individuales aceptados por el lenguaje
                     ## Lectura plana hasta encontrar otros estados
 
-                    if ord(char) == 10:
+                    if ord(data[i]) == 10:
                         line += 1 ## Nueva línea
                         column = 0 ## Reiniciamos la columna
                         column_tk = 0
+                        i += 1
 
-                    elif ord(char) in charcodes_main:
+                    elif ord(data[i]) in charcodes_main:
                         column += 1
                         column_tk = column - 1
-                        temp = char
+                        temp = data[i]
                         print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
+                        i += 1
 
-                    elif ord(char) == 32:
+                    elif ord(data[i]) == 32:
                         ## whitespace
                         column += 1
                         column_tk = column - 1
+                        i += 1
 
-                    elif ord(char) == 39:
+                    elif ord(data[i]) == 39:
                         ## '
                         column += 1
                         column_tk = column - 1
                         state = 1
-                        temp = char
+                        temp = data[i]
+                        i += 1
 
-                    elif (ord(char) >= 97 and ord(char) <= 122) or ord(char) == 241:
+                    elif (ord(data[i]) >= 97 and ord(data[i]) <= 122) or ord(data[i]) == 241:
                         ## a-z ñ included
                         column += 1
                         column_tk = column - 1
                         state = 3
-                        temp = char
+                        temp = data[i]
+                        i += 1
 
-                    elif ord(char) >= 48 and ord(char) <= 57:
+                    elif ord(data[i]) >= 48 and ord(data[i]) <= 57:
                         ## 0-9
                         column += 1
                         column_tk = column - 1
                         state = 4
-                        temp = char
+                        temp = data[i]
+                        i += 1
 
                     else:
                         ## estado de error
                         state = 0
                         column += 1
+                        i += 1
 
                 elif state == 1:
                     ## CADENA
 
-                    if ord(char) == 39:
+                    if ord(data[i]) == 39:
                         ## '
                         column += 1
                         state = 2
-                        temp += char
+                        temp += data[i]
                         print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
+                        i += 1
                     
-                    elif ord(char) == 10:
-                        ## Salto de línea
+                    elif ord(data[i]) == 10:
+                        ## ESTADO DE ERROR POR SALTO DE LÍNEA
+                        state = 0
                         column = 0
                         line += 1
-                        temp += char
+                        i += 1
 
                     else:
                         ## CUALQUIER CARACTER
                         column += 1
-                        temp += char
+                        temp += data[i]
+                        i += 1
 
                 elif state == 2:
                     ## ESTADO DE ACEPTACIÓN DE LA CADENA
-
-                    if ord(char) == 10:
-                        ## Salto de línea
-                        state = 0
-                        column = 0
-                        line += 1
-
-                    elif ord(char) == 32:
-                        ## whitespace
-                        state = 0
-                        column += 1
-                        column_tk = column - 1
-
-                    elif ord(char) in charcodes_after_string:
-                        state = 0
-                        column += 1
-                        column_tk = column - 1
-                        temp = char
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-
-                    else:
-                        ## ESTADO DE ERROR
-                        state = 0
-                        column += 1
-                        column_tk = column
+                    state = 0
 
                 elif state == 3:
                     ## IDENTIFICADOR Y ESTADO DE ACEPTACIÓN DEL IDENTIFICADOR
 
-                    if ord(char) >= 48 and ord(char) <= 57:
+                    if ord(data[i]) >= 48 and ord(data[i]) <= 57:
                         ## DIGITO
                         column += 1
-                        temp += char
+                        temp += data[i]
+                        i += 1
 
-                    elif (ord(char) >= 97 and ord(char) <= 122) or ord(char) == 241:
+                    elif (ord(data[i]) >= 97 and ord(data[i]) <= 122) or ord(data[i]) == 241:
                         ## a-z ñ included
                         column += 1
-                        temp += char
+                        temp += data[i]
+                        i += 1
 
-                    elif ord(char) == 95:
+                    elif ord(data[i]) == 95:
                         ## _
                         column += 1
-                        temp += char
+                        temp += data[i]
+                        i += 1
 
-                    elif ord(char) == 32:
-                        ## CAMBIO DE ESTADO CON ESPACIO
+                    elif ord(data[i]) not in charcodes_continue:
+                        ## ESTADO DE ERROR
+                        print(temp + ": " + str(ord(data[i])))
                         state = 0
                         column += 1
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
                         column_tk = column
-
-                    elif ord(char) == 10:
-                        ## CAMBIO DE ESTADO Y SALTO DE LÍNEA
-                        state = 0
-                        line += 1
-                        column = 0
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-                        column_tk = column
-
-                    elif ord(char) in charcodes_after_id:
-                        column += 1
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-                        column_tk = column - 1
-                        temp = char
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-                        state = 0
+                        i += 1
 
                     else:
-                        ## ESTADO DE ERROR
+                        ## CAMBIO DE ESTADO POR DELIMITADOR ACEPTADO
                         state = 0
-                        column += 1
-                        column_tk = column
+                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
 
                 elif state == 4:
                     ## DIGITO
 
-                    if ord(char) >= 48 and ord(char) <= 57:
+                    if ord(data[i]) >= 48 and ord(data[i]) <= 57:
                         ## DIGITO
                         column += 1
-                        temp += char
+                        temp += data[i]
+                        i += 1
 
-                    elif ord(char) == 46:
+                    elif ord(data[i]) == 46:
                         ## .
                         column += 1
                         state = 5
-                        temp += char
+                        temp += data[i]
+                        i += 1
 
-                    elif ord(char) == 32:
-                        ## CAMBIO DE ESTADO CON ESPACIO
-                        state = 0
-                        column += 1
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-                        column_tk = column
-
-                    elif ord(char) == 10:
-                        ## CAMBIO DE ESTADO Y SALTO DE LÍNEA
-                        state = 0
-                        line += 1
-                        column = 0
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-                        column_tk = column
-
-                    elif ord(char) in charcodes_after_digit:
-                        column += 1
-                        state = 0
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-                        column_tk = column - 1
-                        temp = char
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-
-                    else:
+                    elif ord(data[i]) not in charcodes_continue:
                         ## ESTADO DE ERROR
                         state = 0
                         column += 1
                         column_tk = column
+                        i += 1
+
+                    else:
+                        ## CAMBIO DE ESTADO POR DELIMITADOR ACEPTADO
+                        state = 0
+                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
 
                 elif state == 5:
                     ## .
 
-                    if ord(char) >= 48 and ord(char) <= 57:
+                    if ord(data[i]) >= 48 and ord(data[i]) <= 57:
                         ## DIGITO
                         column += 1
                         state = 6
-                        temp += char
+                        temp += data[i]
+                        i += 1
 
                     else:
                         ## ESTADO DE ERROR
                         state = 0
                         column += 1
                         column_tk = column
+                        i += 1
 
                 elif state == 6:
 
-                    if ord(char) >= 48 and ord(char) <= 57:
+                    if ord(data[i]) >= 48 and ord(data[i]) <= 57:
                         ## DIGITO
                         column += 1
-                        temp += char
+                        temp += data[i]
+                        i += 1
 
-                    elif ord(char) == 32:
-                        ## CAMBIO DE ESTADO CON ESPACIO
-                        state = 0
-                        column += 1
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-                        column_tk = column
-
-                    elif ord(char) == 10:
-                        ## CAMBIO DE ESTADO Y SALTO DE LÍNEA
-                        state = 0
-                        line += 1
-                        column = 0
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-                        column_tk = column
-
-                    elif ord(char) in charcodes_after_digit:
-                        column += 1
-                        state = 0
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-                        column_tk = column - 1
-                        temp = char
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
-
-                    else:
+                    elif ord(data[i]) not in charcodes_continue:
                         ## ESTADO DE ERROR
                         state = 0
                         column += 1
                         column_tk = column
+                        i += 1
+
+                    else:
+                        ## CAMBIO DE ESTADO POR DELIMITADOR ACEPTADO
+                        state = 0
+                        print(temp + " linea: " + str(line) + ", columna: " + str(column_tk))
 
                 else:
                     ## ESTADO DE ERROR
                     state = 0
                     column += 1
                     column_tk = column - 1
+                    i += 1
